77fd277daf0cdf07e8a2cad567f5e39c4e182df4
unknown <cubobale1423@gmail.com>
2022-12-20 00:44:33 -0500

    documentacion de codigo

--- config/Enrutador.php
+++ config/Enrutador.php
@@ -4,32 +4,34 @@
 
     public static function run(Request $request){
 
-        $controlador = $request->getControlador() . "Controller";
-        $ruta = ROOT . "Controllers" . DS . $controlador . ".php";
-        $metodo = $request->getMetodo();
+        $controlador = $request->getControlador() . "Controller";//obtenemos el controlador con la funcion getControlador 
+        $ruta = ROOT . "Controllers" . DS . $controlador . ".php";//creamos una ruta 
+        $metodo = $request->getMetodo();//obtenemos el metodo de la funcion getMetodo
 
         if ($metodo == "index.php") {
             $metodo = "index";
         }
-
-        $argumento = $request->getArgumento();
-        if (is_readable($ruta)) {
+        $argumento = $request->getArgumento();//obtenemos el argumento de la funcion getArgumento
+        if (is_readable($ruta)) {//si la ruta es leible sera true 
             require_once $ruta;
             $mostrar = "Controllers\\". $controlador;
             $controlador = new $mostrar;
-            if (!isset($argumento)) {
-               $datos =  call_user_func(array($controlador, $metodo));
+            if (!isset($argumento)) {//si el argumento no existe
+                //call_user_func = Llamar a una llamada de retorno dada por el primer parámetro
+               $datos =  call_user_func(array($controlador, $metodo));//array crea un array
             }else{
+                //call_user_func = Llamar a una llamada de retorno dada por el primer parámetro
                $datos = call_user_func_array(array($controlador, $metodo), $argumento);
             }
         }   
         
-       //cargar vistas
+       //carga las vista para el usuario
+       //creamos una ruta
         $ruta  = ROOT ."views". DS .$request->getControlador(). DS .$request->getMetodo(). ".php";
-        if (is_readable($ruta)) {
+        if (is_readable($ruta)){//si el archivo es leible sera true
             include_once $ruta;
         }else{
-            print "no se puede";
+            print "no se puede cargar la vista";
         }
     }
 }
